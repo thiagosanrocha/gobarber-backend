7d804047e88ec8d6dd8727a07b4a306ba10b523a
Thiago Rocha <thiagorocha010@gmail.com>
2021-01-22 23:45:08 -0300

    chore: avatar upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,25 @@
+// Modules
 import { Router } from 'express';
+import multer, { diskStorage } from 'multer';
+import { resolve } from 'path';
+import { v4 as uuid } from 'uuid';
 
+// Services
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthentication from '../middlewares/ensureAuthentication';
 
 const usersRouter = Router();
+
+const upload = multer({
+  storage: diskStorage({
+    destination: resolve(__dirname, '..', '..', 'tmp'),
+    filename: (request, file, callback) => {
+      const fileName = `${uuid().replace('-', '')}-${file.originalname}`;
+
+      callback(null, fileName);
+    },
+  }),
+});
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -21,5 +38,15 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthentication,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
